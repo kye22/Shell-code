009fd6e63b70e3ae93b12748f85a7b19de1ffe5a
Cayetano_Lopez_Moreno <cayetanolopez@uma.es>
2024-12-09 18:13:48 -0500

    añadimos algo

--- Shell_project.c
+++ Shell_project.c
@@ -16,7 +16,7 @@ To compile and run the program:
 
 #include "job_control.h"   // remember to compile with module job_control.c 
 #include <string.h>
-
+newlist(background);
 #define MAX_LINE 256 /* 256 chars per line, per command, should be enough. */
 // -----------------------------------------------------------------------
 //                            Manejador de childs          
@@ -26,33 +26,34 @@ void child_handler (int s)
 {
 	int pid_wait;
 	int status;
-	pid_wait = waitpid(-1, &status, WNOHANG | WUNTRACED );
-	if (pid_wait == 0) {
-		printf("manejador SIGCHLD, ningún cambio en los procesos");
-		return; /*no hay cambio del proceso que envia la señal*/
+	while (1){
+		pid_wait = waitpid(-1, &status, WNOHANG | WUNTRACED | __W_CONTINUED );
+		if (pid_wait == 0) {
+			printf("manejador SIGCHLD, ningún cambio en los procesos");
+			return; /*no hay cambio del proceso que envia la señal*/
+		}
+		if (pid_wait == -1) return; /*error*/
+		if (WIFEXITED(status)){
+							/*teminó con exit()*/
+							/*WEXITSTATUS(status)*/
+			printf("\n Background pid: %d,Finished, info: %d\n", pid_wait,  WEXITSTATUS(status));
+		}else if(WIFSIGNALED(status)){
+			/*terminó por una señal*/
+			/*WTERMSIG(status)*/
+			printf("\n Signaled child pid: %d,  %d\n", pid_wait, WTERMSIG(status));
+		}else if (WCOREDUMP(status)){
+							
+		}else if(WIFSTOPPED(status)){
+			/*stopped*/
+			/*WSTOPSIG(status)*/
+			printf("\nStopped pid: %d, %d\n", pid_wait, WSTOPSIG(status));
+		}else if (WIFCONTINUED(status)){
+			/**/
+			printf("Continued %d\n", pid_wait);
+		}else{
+			/*pid_wait == -1*/
+		}
 	}
-	if (pid_wait == -1) return; /*error*/
-	if (WIFEXITED(status)){
-						/*teminó con exit()*/
-						/*WEXITSTATUS(status)*/
-		printf("\n Background pid: %d,Finished, info: %d\n", pid_wait,  WEXITSTATUS(status));
-	}else if(WIFSIGNALED(status)){
-		/*terminó por una señal*/
-		/*WTERMSIG(status)*/
-		printf("\n Signaled child pid: %d,  %d\n", pid_wait, WTERMSIG(status));
-	}else if (WCOREDUMP(status)){
-						
-	}else if(WIFSTOPPED(status)){
-		/*stopped*/
-		/*WSTOPSIG(status)*/
-		printf("\nStopped pid: %d, %d\n", pid_wait, WSTOPSIG(status));
-	}else if (WIFCONTINUED(status)){
-		/**/
-		printf("Continued %d\n", pid_wait);
-	}else{
-		/*pid_wait == -1*/
-	}
-
 }
 
 // -----------------------------------------------------------------------
@@ -70,12 +71,12 @@ int main(int argc, char *argv[], char *env[])
 	int status;             /* status returned by wait */
 	enum status status_res; /* status processed by analyze_status() */
 	int info;				/* info processed by analyze_status() */
-
+	sigset_t mySet;
 	while (1)   /* Program terminates normally inside get_command() after ^D is typed*/
 	{   		
 		ignore_terminal_signals();
 		signal(SIGCHLD, child_handler);
-		printf("COMMAND->");
+		printf("COMMAND-> ");
 		fflush(stdout);
 		get_command(inputBuffer, MAX_LINE, args, &background);  /* get next command */
 		
@@ -83,6 +84,14 @@ int main(int argc, char *argv[], char *env[])
 
 		if (strcmp(args[0], "hola")==0){
 			printf("que tal?\n");
+			continue;
+		} else if (strcmp(args[0], "quit") == 0){
+			break;
+		}else if (strcmp(args[0], "maskCHLD") == 0){
+			block_SIGCHLD();
+			continue;
+		}else if (strcmp(args[0], "unmaskCHLD") == 0){
+			unblock_SIGCHLD();
 			continue;
 		}
 
@@ -133,8 +142,8 @@ int main(int argc, char *argv[], char *env[])
 					
 					
 				}else{
-
 					printf("\nBackground job running... pid: %d , command: %s\n", pid_fork, args[0]);
+
 				}
 				
 				break;
